b9f6437033545a67352a1a9b0fb372d4bd052ddf
Diego Henrique <dieguinho251997@gmail.com>
2025-10-24 22:12:40 -0300

    Adicionei alguns breakpoints para responsividade e função de apagar.

--- 02 CÓDIGO/src/calculadora.js
+++ 02 CÓDIGO/src/calculadora.js
@@ -1,3 +1,5 @@
+// trago todos os elementos que quero usar para aqui.
+
 let botoes_numericos = document.querySelectorAll(".botao_numerico");
 
 const tela = document.querySelector(".valor_saida");
@@ -6,6 +8,8 @@ const resultado = document.querySelector(".botao_resultado");
 
 const historico = document.querySelector(".valor_historico");
 
+
+// seleciono todos os botões de números e operadores para pegar os valores digitados.
 botoes_numericos.forEach (botao => {
     botao.addEventListener("click" , () => {
         const valor = botao.innerText;
@@ -18,6 +22,7 @@ botoes_numericos.forEach (botao => {
     })
 });
 
+// calculo o resultado e transmito para a tela de exibição.
 resultado.addEventListener("click" , ()=> {
     if(tela.innerText === "") {
         tela.innerText = "";
@@ -31,10 +36,18 @@ resultado.addEventListener("click" , ()=> {
     }
 });
 
-document.querySelector("#botao_c").addEventListener("click" , function() {
+// seleciono o botão de apagar para cada valor digitado. 
+document.querySelector("#botao_apagar").addEventListener("click", () => {
+        if(tela.innerText === "") {
+            tela.innerText = "";
+        } else {
+            let apagar = tela.innerText.slice(0, -1);
+            tela.innerText = apagar;
+        }
+})
+
+// seleciono o botão de apagar tudo de uma vez na tela de exibição.
+document.querySelector("#botao_c").addEventListener("click" , () => {
     tela.innerText = "";
     historico.innerText = "";
 });
-
-
-
